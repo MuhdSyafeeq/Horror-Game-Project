e8e22366b82f195c2f152d163130b0dc10fe5f39
MuhdSyafeeq <fauzisyafeeq@gmail.com>
2021-04-12 16:08:43 +0800

    - Adaptation Environment
    - Removed Unused Variable
    - Added Sided Environment to adapt the world environment

--- GameProjects1/Assets/ActiveKeysMovement.cs
+++ GameProjects1/Assets/ActiveKeysMovement.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class ActiveKeysMovement : MonoBehaviour
 {
-    public CharacterController controller;
     public float speedModifier = 1.2f;
 
     // Update is called once per frame
@@ -12,6 +11,7 @@ public class ActiveKeysMovement : MonoBehaviour
     {
         // Flash Sensory (Reduce All Audio to 0.5) (Create Emmitter of Blink when any entity moves)
         if(Input.GetKeyDown(KeyCode.F)) { Debug.Log("Active/Diactive Flash Sensory!"); }
+        if (Input.GetKeyDown(KeyCode.Q)) { Debug.Log("Active/Diactive Hearing Ultra-Sensor!"); }
         if (Input.GetKeyDown(KeyCode.LeftShift)) { GetComponent<PlayerMovement>().speed *= speedModifier; }
         if (Input.GetKeyUp(KeyCode.LeftShift)) { GetComponent<PlayerMovement>().speed /= speedModifier; }
     }
