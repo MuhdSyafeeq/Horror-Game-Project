e25c456830f571dd04004706f52ce09b2789f66c
MuhdSyafeeq <fauzisyafeeq@gmail.com>
2021-07-12 19:43:18 +0800

    Added Room
    - Starting Point
    -- Next will be Living Room and Entrance to Main Events (ELEVATOR for NEXT SEQUENCE)

---
+++ GameProjects1/Assets/ActionObjects.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class ActionObjects : MonoBehaviour
+{
+    public GameObject inRangeObjects;
+    public GameObject player;
+    public bool playerEntered;
+
+    public bool getBoolean()
+    {
+        return playerEntered;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (playerEntered)
+        {
+            inRangeObjects.GetComponent<BoxCollider>().enabled = true;
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerEntered = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerEntered = false;
+        }
+    }
+}
---
+++ GameProjects1/Assets/InteractionObjects.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractionObjects : MonoBehaviour
+{
+    public float range = 100f;
+
+    public Camera fpsCam;
+
+    private bool showInteractMsg;
+    private GUIStyle guiStyle;
+    private string msg;
+
+    void Start()
+    {
+        //setup GUI style settings for user prompts
+        setupGui();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            if (hit.collider.gameObject.layer == 10)
+            {
+                Debug.Log(hit.collider.name);
+                showInteractMsg = true;
+            }
+            else
+            {
+                showInteractMsg = false;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            InteractUse();
+        }
+    }
+
+    void InteractUse()
+    {
+        
+    }
+
+    #region GUI Config
+
+    //configure the style of the GUI
+    private void setupGui()
+    {
+        guiStyle = new GUIStyle();
+        guiStyle.fontSize = 16;
+        guiStyle.fontStyle = FontStyle.Bold;
+        guiStyle.normal.textColor = Color.white;
+        msg = "Press E to Interact";
+    }
+
+    private string getGuiMsg(bool isOpen)
+    {
+        string rtnVal;
+        if (isOpen)
+        {
+            rtnVal = "E to turn on Lights";
+        }
+        else
+        {
+            rtnVal = "E to turn off Lights";
+        }
+
+        return rtnVal;
+    }
+
+    void OnGUI()
+    {
+        if (showInteractMsg)  //show on-screen prompts to user for guide.
+        {
+            GUI.Label(new Rect(50, Screen.height - 50, 200, 50), msg, guiStyle);
+        }
+    }
+    //End of GUI Config --------------
+    #endregion
+}
