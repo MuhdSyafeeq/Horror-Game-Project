5c501f62ed275231adb05509a55a1c04c2eef60e
MuhdSyafeeq <fauzisyafeeq@gmail.com>
2021-07-22 16:59:21 +0800

    Adjusting Minor Fix
    Fixing Some Interaction Problems
    - Need to Do Kitchen!!!

--- GameProjects1/Assets/ActionObjects.cs
+++ GameProjects1/Assets/ActionObjects.cs
@@ -4,6 +4,7 @@ public class ActionObjects : MonoBehaviour
 {
     public GameObject inRangeObjects;
     public GameObject refObjects;
+    public GameObject linkedInteraction;
     public InteractionObjects iObj;
 
     public string currMsg = "Press E to Interact";
@@ -38,8 +39,8 @@ public class ActionObjects : MonoBehaviour
             fl.transform.SetParent(iObj.hands.transform);
             fl.transform.localPosition = Vector3.zero;
             fl.transform.localRotation = Quaternion.identity;
-            fl.transform.localEulerAngles = new Vector3(90, 0, 0);
-            fl.transform.localScale = new Vector3(5, 5, 5);
+            //fl.transform.localEulerAngles = new Vector3(90, 0, 0);
+            //fl.transform.localScale = new Vector3(5, 5, 5);
         }
 
         else
@@ -49,7 +50,7 @@ public class ActionObjects : MonoBehaviour
                 refObjects.GetComponent<Animator>().enabled = true;
                 string animationName = "isOpen_Obj_";
 
-                if(iObj.theHitObj.name == "tvStandDoor_R")
+                if(iObj.theHitObj.name == "tvStandDoor_R") //|| iObj.theHitObj.name == "doorR")
                 {
                     animationName = animationName + "2";
                 }
@@ -57,16 +58,37 @@ public class ActionObjects : MonoBehaviour
                 {
                     animationName = animationName + "1";
                 }
+                /*
+                    int count = 0;
+                    foreach(AnimatorControllerParameter param_ in refObjects.GetComponent<Animator>().parameters)
+                    {
+                        //Debug.Log($"Comparing --> {animationName} << >> {param_.name}");
+                        if(animationName == param_.name) { break; }
+                        count++;
+                    }
 
+                    if(count == refObjects.GetComponent<Animator>().parameterCount)
+                    {
+                        animationName = "isOpen_Obj_1";
+                    }
+                */
                 if( refObjects.GetComponent<Animator>().GetBool(animationName) == false )
                 {
                     refObjects.GetComponent<Animator>().SetBool(animationName, true);
                     currMsg = "Press E to Close";
+                    if (iObj.theHitObj.name == "doorR" || iObj.theHitObj.name == "doorL")
+                    {
+                        linkedInteraction.GetComponent<ActionObjects>().currMsg = this.currMsg;
+                    }
                 }
                 else
                 {
                     refObjects.GetComponent<Animator>().SetBool(animationName, false);
                     currMsg = "Press E to Open";
+                    if (iObj.theHitObj.name == "doorR" || iObj.theHitObj.name == "doorL")
+                    {
+                        linkedInteraction.GetComponent<ActionObjects>().currMsg = this.currMsg;
+                    }
                 }
             }
             else
