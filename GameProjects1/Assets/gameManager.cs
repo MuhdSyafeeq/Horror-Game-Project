8bb74a5cc5045f401b48a8c6cde2e1e1f77065e7
MuhdSyafeeq <fauzisyafeeq@gmail.com>
2021-09-28 15:16:11 +0800

    Light Now Regain Sanity (part 1)
    - Sanity Now can be Regain and Reduced
    - - Sanity Depends on Light
    *** Need to do ***
    - Game Over Function
    - Pause Function

--- GameProjects1/Assets/gameManager.cs
+++ GameProjects1/Assets/gameManager.cs
@@ -11,9 +11,11 @@ public class gameManager : MonoBehaviour
 
     public void Pause()
     {
+        Time.timeScale = 0f;
     }
 
     public void Resume()
     {
+        Time.timeScale = 1f;
     }
 }
