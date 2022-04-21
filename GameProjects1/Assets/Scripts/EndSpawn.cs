48c7fb73acda783aff67e83a8fbca599e57d99a6
MuhdSyafeeq <fauzisyafeeq@gmail.com>
2022-04-21 15:11:32 +0800

    Update 0.1.2a
    Location for Random Spawn added
    - Need to add more Random Location to Spawn
    
    Small Fixes
    - Replacing box collider for all curtains
    
    Required to Update
    - Need to add Main Menu Screen***
    - Need to add audio footstep***
    - Distortion Reality (If sanity is too low)***
    
    Misc
    - Need to update crawler (New entity to chase)
    - Optimizing the game

---
+++ GameProjects1/Assets/Scripts/EndSpawn.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class EndSpawn : MonoBehaviour
+{
+    [Header("Game Event Items")]
+    [SerializeField]
+    public CapsuleCollider capsuleCollider; 
+    [Space(1)]
+    //public GameObject[] itemParts;
+    public GameObject[] aura;
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("FINSIHED");
+        gameManager.Instance.Finish();
+    }
+
+    private void Start()
+    {
+        if(capsuleCollider == null) { capsuleCollider = this.GetComponent<CapsuleCollider>(); capsuleCollider.enabled = false; }
+    }
+
+    private void Update()
+    {
+        if(gameManager.Instance.isPaused != true)
+        {
+            if (gameManager.Instance.missingparts == 6)
+            {
+                capsuleCollider.enabled = true;
+                foreach (GameObject auras in aura)
+                {
+                    auras.SetActive(true);
+                }
+            }
+        }
+    }
+}
