e787e6c3d554cd58053e6ac280a6ba896827673a
MuhdSyafeeq <fauzisyafeeq@gmail.com>
2022-04-24 17:42:23 +0800

    Update 0.1.2b
    Main Menu Fixes
    - Added Script for Eye Tracking Mouse
    - Added Interactions (No Button Functions)
    
    Small Fixes
    - Static Object On Locations
    
    Required to Update
    - Need to add Main Menu Screen***
    - Need to add audio footstep***
    - Distortion Reality (If sanity is too low)***
    
    Misc
    - Need to update crawler (New entity to chase)
    - Optimizing the game

---
+++ GameProjects1/Assets/eyetracker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eyetracker : MonoBehaviour
+{
+    public MouseLook getData;
+
+    // Update is called once per frame
+    void FixedUpdate(){
+        float mouseX = Input.GetAxis("Mouse X") * getData.mouseSensitiviy * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * getData.mouseSensitiviy * Time.deltaTime;
+        
+        transform.LookAt(new Vector3(mouseX, mouseY));
+
+        // Look Left or Right (Side-to-Side)
+        //playerBody.Rotate(Vector3.up * mouseX);
+
+        // Look Up or Down (No-Clamping Method)
+        //xRotation -= mouseY;
+        //xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        //transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
